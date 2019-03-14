1d486ff3e5f678ff1b064ed5085fa2a589047ff3
Sam Yi <swzy92@gmail.com>
2019-03-14 12:36:25 -0400

    Working example with ARCore library

--- app/src/main/java/com/example/android/tether/DemoUtils.java
+++ app/src/main/java/com/example/android/tether/DemoUtils.java
@@ -1,5 +1,3 @@
-package com.example.android.tether;
-
 /*
  * Copyright 2018 Google LLC
  *
@@ -15,6 +13,7 @@ package com.example.android.tether;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.tether;
 
 import android.app.Activity;
 import android.content.Context;
@@ -38,72 +37,73 @@ import uk.co.appoly.arcorelocation.utils.ARLocationPermissionHelper;
 
 /** Static utility methods to simplify creating multiple demo activities. */
 public class DemoUtils {
-    private static final String TAG = "SceneformDemoUtils";
-    /**
-     * Creates and shows a Toast containing an error message. If there was an exception passed in it
-     * will be appended to the toast. The error will also be written to the Log
-     */
-    public static void displayError(
-            final Context context, final String errorMsg, @Nullable final Throwable problem) {
-        final String tag = context.getClass().getSimpleName();
-        final String toastText;
-        if (problem != null && problem.getMessage() != null) {
-            Log.e(tag, errorMsg, problem);
-            toastText = errorMsg + ": " + problem.getMessage();
-        } else if (problem != null) {
-            Log.e(tag, errorMsg, problem);
-            toastText = errorMsg;
-        } else {
-            Log.e(tag, errorMsg);
-            toastText = errorMsg;
-        }
-
-        new Handler(Looper.getMainLooper())
-                .post(
-                        () -> {
-                            Toast toast = Toast.makeText(context, toastText, Toast.LENGTH_LONG);
-                            toast.setGravity(Gravity.CENTER, 0, 0);
-                            toast.show();
-                        });
+  private static final String TAG = "SceneformDemoUtils";
+  /**
+   * Creates and shows a Toast containing an error message. If there was an exception passed in it
+   * will be appended to the toast. The error will also be written to the Log
+   */
+  public static void displayError(
+      final Context context, final String errorMsg, @Nullable final Throwable problem) {
+    final String tag = context.getClass().getSimpleName();
+    final String toastText;
+    if (problem != null && problem.getMessage() != null) {
+      Log.e(tag, errorMsg, problem);
+      toastText = errorMsg + ": " + problem.getMessage();
+    } else if (problem != null) {
+      Log.e(tag, errorMsg, problem);
+      toastText = errorMsg;
+    } else {
+      Log.e(tag, errorMsg);
+      toastText = errorMsg;
     }
 
-    /**
-     * Creates an ARCore session. This checks for the CAMERA permission, and if granted, checks the
-     * state of the ARCore installation. If there is a problem an exception is thrown. Care must be
-     * taken to update the installRequested flag as needed to avoid an infinite checking loop. It
-     * should be set to true if null is returned from this method, and called again when the
-     * application is resumed.
-     *
-     * @param activity - the activity currently active.
-     * @param installRequested - the indicator for ARCore that when checking the state of ARCore, if
-     *     an installation was already requested. This is true if this method previously returned
-     *     null. and the camera permission has been granted.
-     */
-    public static Session createArSession(Activity activity, boolean installRequested)
-            throws UnavailableException {
-        Session session = null;
-        // if we have the camera permission, create the session
-        if (ARLocationPermissionHelper.hasPermission(activity)) {
-            switch (ArCoreApk.getInstance().requestInstall(activity, !installRequested)) {
-                case INSTALL_REQUESTED:
-                    return null;
-                case INSTALLED:
-                    break;
-            }
-            session = new Session(activity);
-            // IMPORTANT!!!  ArSceneView needs to use the non-blocking update mode.
-            Config config = new Config(session);
-            config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
-            session.configure(config);
-        }
-        return session;
-    }
+    new Handler(Looper.getMainLooper())
+        .post(
+            () -> {
+              Toast toast = Toast.makeText(context, toastText, Toast.LENGTH_LONG);
+              toast.setGravity(Gravity.CENTER, 0, 0);
+              toast.show();
+            });
+  }
 
-    /** Check to see we have the necessary permissions for this app, and ask for them if we don't. */
+  /**
+   * Creates an ARCore session. This checks for the CAMERA permission, and if granted, checks the
+   * state of the ARCore installation. If there is a problem an exception is thrown. Care must be
+   * taken to update the installRequested flag as needed to avoid an infinite checking loop. It
+   * should be set to true if null is returned from this method, and called again when the
+   * application is resumed.
+   *
+   * @param activity - the activity currently active.
+   * @param installRequested - the indicator for ARCore that when checking the state of ARCore, if
+   *     an installation was already requested. This is true if this method previously returned
+   *     null. and the camera permission has been granted.
+   */
+  public static Session createArSession(Activity activity, boolean installRequested)
+      throws UnavailableException {
+    Session session = null;
+    // if we have the camera permission, create the session
+    if (ARLocationPermissionHelper.hasPermission(activity)) {
+      switch (ArCoreApk.getInstance().requestInstall(activity, !installRequested)) {
+        case INSTALL_REQUESTED:
+          return null;
+        case INSTALLED:
+          break;
+      }
+      session = new Session(activity);
+      // IMPORTANT!!!  ArSceneView needs to use the non-blocking update mode.
+      Config config = new Config(session);
+      config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+      session.configure(config);
+    }
+    return session;
+  }
+
+  /** Check to see we have the necessary permissions for this app, and ask for them if we don't. */
   /*public static void requestPermission(Activity activity, int requestCode) {
     ActivityCompat.requestPermissions(
         activity, new String[] {Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION}, requestCode);
   }
+
   *//** Check to see we have the necessary permissions for this app. *//*
   public static boolean hasPermission(Activity activity) {
     return ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA)
@@ -114,6 +114,7 @@ public class DemoUtils {
     return ActivityCompat.shouldShowRequestPermissionRationale(
         activity, Manifest.permission.CAMERA);
   }
+
   *//** Launch Application Setting to grant permission. *//*
   public static void launchPermissionSettings(Activity activity) {
     Intent intent = new Intent();
@@ -122,22 +123,22 @@ public class DemoUtils {
     activity.startActivity(intent);
   }*/
 
-    public static void handleSessionException(
-            Activity activity, UnavailableException sessionException) {
+  public static void handleSessionException(
+      Activity activity, UnavailableException sessionException) {
 
-        String message;
-        if (sessionException instanceof UnavailableArcoreNotInstalledException) {
-            message = "Please install ARCore";
-        } else if (sessionException instanceof UnavailableApkTooOldException) {
-            message = "Please update ARCore";
-        } else if (sessionException instanceof UnavailableSdkTooOldException) {
-            message = "Please update this app";
-        } else if (sessionException instanceof UnavailableDeviceNotCompatibleException) {
-            message = "This device does not support AR";
-        } else {
-            message = "Failed to create AR session";
-            Log.e(TAG, "Exception: " + sessionException);
-        }
-        Toast.makeText(activity, message, Toast.LENGTH_LONG).show();
+    String message;
+    if (sessionException instanceof UnavailableArcoreNotInstalledException) {
+      message = "Please install ARCore";
+    } else if (sessionException instanceof UnavailableApkTooOldException) {
+      message = "Please update ARCore";
+    } else if (sessionException instanceof UnavailableSdkTooOldException) {
+      message = "Please update this app";
+    } else if (sessionException instanceof UnavailableDeviceNotCompatibleException) {
+      message = "This device does not support AR";
+    } else {
+      message = "Failed to create AR session";
+      Log.e(TAG, "Exception: " + sessionException);
     }
+    Toast.makeText(activity, message, Toast.LENGTH_LONG).show();
+  }
 }
